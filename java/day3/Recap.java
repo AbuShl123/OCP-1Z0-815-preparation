e932465bc11560872515908818fc48b9ac586573
AbuShl123 <shoalievabubakr@gmail.com>
2023-07-04 23:43:20 +0600

    Java Identifier rules

---
+++ java/day3/Recap.java
@@ -0,0 +1,11 @@
+package day3;
+
+public class Recap {
+   public static void main(String[] args) {
+      int a = 017; 
+      int b = 0xa1;
+      int c = 0b1011;
+
+      System.out.println(a + "\n" + b + "\n" + c);
+   }
+}
